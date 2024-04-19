0b20dcd74548e408730721bfb8820ad6b51d92b3
Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
2024-04-19 20:40:06 +0800

    Commit Text Recog App

---
+++ app/src/main/java/com/dicoding/textrecog/ResultActivity.kt
@@ -0,0 +1,36 @@
+package com.dicoding.textrecog
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.textrecog.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageUri = Uri.parse(intent.getStringExtra(EXTRA_IMAGE_URI))
+        imageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.resultImage.setImageURI(it)
+        }
+
+        val detectedText = intent.getStringExtra(EXTRA_RESULT)
+        binding.resultText.text = detectedText
+
+    }
+
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image_uri"
+        const val EXTRA_RESULT = "extra_result"
+    }
+
+}
